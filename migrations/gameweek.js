6218eb30b583d538250477bf861f080662208e94
Matthew Dodd <matthew.dodd93@gmail.com>
2021-02-04 19:06:05 +0000

    add post id to gameweek

--- migrations/gameweek.js
+++ migrations/gameweek.js
@@ -3,6 +3,7 @@ const knex = require('../config/database');
 knex.schema.createTable('gameweek', (table) => {
     table.increments('id');
     table.string('name');
+    table.string('post_id');
     table.integer('gameweek_id');
     table.datetime('deadline_time');
 }).then(() => {
