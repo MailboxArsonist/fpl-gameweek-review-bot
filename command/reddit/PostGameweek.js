e7d225ce8a4ca30285facaaea5de02c60971a63d
Matthew Dodd <matthew.dodd93@gmail.com>
2021-02-04 19:06:34 +0000

    wip PostGameweek, add fixtures, players

--- command/reddit/PostGameweek.js
+++ command/reddit/PostGameweek.js
@@ -16,6 +16,9 @@ class CreateGameweek {
         this.body = gwBody;
     }
 
+    /**
+     * inits the reddit snoowrap wrapper
+     */
     initRedditWrapper(){
         this.redditWrapper = new snoowrap({
             userAgent: process.env.REDDIT_USERAGENT,
@@ -26,14 +29,56 @@ class CreateGameweek {
         });
     }
 
+    /**
+     * post the gameweek onto subreddit
+     */
     async postGameweek(){
-        const gw = await this.getGameweek(this.gameweekId);
-        const formattedTitle = this.title.replace('%GW%', this.gameweekId).replace('%SEASON%', '(2019/2020)');
+        const { post_id } = await this.getGameweek(this.gameweekId);
+        const fixtures = await this.getFixturesWithTeams(this.gameweekId);
+        console.log(fixtures);
 
-        this.redditWrapper.getSubreddit('test').submitSelfpost({
+        // check if its already been posted to reddit.
+        if(post_id !== null) {
+            throw new Error(`GW ${this.gameweekId} has already been posted on subreddit with the ID: ${post_id}`);
+        }
+
+        const formattedTitle = this.title.replace('%GW%', this.gameweekId).replace('%SEASON%', '(2020/2021)');
+
+        // post onto subreddit
+        const submission = await this.redditWrapper.getSubreddit('fpl_test_bot').submitSelfpost({
             title: formattedTitle,
-            text: this.gwBody,
+            text: this.body,
+        })
+
+        // fetch the posted submission, need to do this to get the ID
+        const fetchedSubmission = await submission.fetch();
+
+        // store the post ID
+        this.setGameweekRedditPost(this.gameweekId, fetchedSubmission.id);
+
+        fixtures.forEach(fixture => {
+            const { away_name, away_id, home_name, home_id } = fixture;
+            const home_players = this.getPlayersByTeam(home_id);
+            const away_players = this.getPlayersByTeam(away_id);
+
+            Promise.all([home_players, away_players]).then(([homePlayers, awayPlayers]) => {
+                console.log(homePlayers);
+                console.log(awayPlayers);
+                // post home team
+                fetchedSubmission.reply(home_name).then(comment => {
+                    console.log(comment);
+                });
+            });
         });
+    }
+
+    setGameweekRedditPost(gameweekId, postId){
+        knex('gameweek')
+            .where('gameweek_id', '=', gameweekId)
+            .update({ 'post_id': postId })
+            .catch(err => {
+                console.error(err);
+            });
     }
 
     /**
@@ -47,6 +92,54 @@ class CreateGameweek {
                 .first()
                 .then(row => {
                     resolve(row);
+                })
+                .catch(err => {
+                    console.error(err);
+                    reject(err);
+                });
+        });
+    }
+
+    /**
+     * @returns {promise}
+     * @param {int} gameweekId - id of the gameweek
+     */
+    getFixturesWithTeams(gameweekId){
+        return new Promise ((resolve, reject) => {
+            knex('fixture')
+                .select(
+                    'away_team.name AS away_name',
+                    'away_team.team_id AS away_id',
+                    'home_team.name AS home_name',
+                    'home_team.team_id AS home_id',
+                )
+                .leftJoin('team as home_team', 'home_team.team_id', 'fixture.home_team_id')
+                .leftJoin('team as away_team', 'away_team.team_id', 'fixture.away_team_id')
+                .where('gameweek_id', '=', gameweekId)
+                .then(rows => {
+                    resolve(rows);
+                })
+                .catch(err => {
+                    console.error(err);
+                    reject(err);
+                });
+        });
+    }
+
+    /**
+     * @returns {promise}
+     * @param {int} teamId - id of the team
+     */
+    getPlayersByTeam(teamId){
+        return new Promise ((resolve, reject) => {
+            knex('player')
+                .select(
+                    'web_name',
+                    'team_id'
+                )
+                .where('team_id', '=', teamId)
+                .then(rows => {
+                    resolve(rows);
                 })
                 .catch(err => {
                     console.error(err);
