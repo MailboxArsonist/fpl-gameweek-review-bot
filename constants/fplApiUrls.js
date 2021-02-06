c64c80c7d063124bf6c02ee546eb42d47d5674d0
Matthew Dodd <matthew.dodd93@gmail.com>
2021-02-06 16:07:46 +0000

    move api urls to their own file

---
+++ constants/fplApiUrls.js
@@ -0,0 +1,17 @@
+const FPL_SUMMARY_API_URL = "https://fantasy.premierleague.com/api/bootstrap-static/";
+const FPL_DETAIL_URL = "https://fantasy.premierleague.com/api/element-summary/{}/";
+const FPL_HISTORY_URL = "https://fantasy.premierleague.com/api/entry/{}/history/";
+const FPL_TEAM_URL = "https://fantasy.premierleague.com/api/entry/{}/event/{}/picks/";
+const FPL_TEAM_TRANSFER_URL = "https://fantasy.premierleague.com/api/entry/{}/transfers/";
+const FPL_LEAGUE_URL = "https://fantasy.premierleague.com/api/leagues-classic/{}/standings/";
+const FPL_FIXTURE_URL = "https://fantasy.premierleague.com/api/fixtures/";
+
+module.exports = {
+    FPL_SUMMARY_API_URL,
+    FPL_DETAIL_URL,
+    FPL_HISTORY_URL,
+    FPL_TEAM_URL,
+    FPL_TEAM_TRANSFER_URL,
+    FPL_LEAGUE_URL,
+    FPL_FIXTURE_URL,
+}
